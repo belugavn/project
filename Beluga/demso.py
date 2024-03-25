69ab21a6254b08a57b202cac3e62de7a3409cd5c
belugavn <anbaocp99@gmail.com>
2024-03-25 22:17:44 +0700

    P

---
+++ Beluga/demso.py
@@ -0,0 +1,47 @@
+a = int(input("Nhap so hang tram"))
+b = int (input("Nhap so hang chuc"))
+c = int(input("Nhap so hang don vi"))
+if a==1:
+  a="Mot tram"
+elif a==2:
+    a="Hai tram"
+elif a==3:
+    a="Ba tram"
+elif a==4:
+    a="Bon tram"
+elif a==5:
+    a="Nam tram"
+elif a==6:
+    a="Sau tram"
+elif a==7:
+    a="'Bay tram"
+elif a==8:
+    a="Tam tram"
+elif a==9:
+    a="Chin tram"
+if b==0:
+    b ="Tram"
+elif b==1:
+    b="Muoi"
+elif b==2:
+    b="Hai Muoi"
+elif b==3:
+    b="Ba Muoi"
+elif b==4:
+    b="Bon Muoi"
+elif b==5:
+    b="Nam Muoi"
+elif b==6:
+    b="Sau Muoi"
+elif b==7:
+    b="Bay Muoi"
+elif b==8:
+    b="Tam Muoi"
+elif b==9:
+    b="Chin muoi"
+if c==0:
+    None
+elif c==1:
+
+
+
